ee1a0ec70aa7dbf2406ca747fb7edff5176561b8
r0x <r0x@r0x.org>
2023-02-21 18:52:46 +0800

    coding broker

--- broker/adaptee/kafka.go
+++ broker/adaptee/kafka.go
@@ -1,37 +1,33 @@
 package adaptee
 
 import (
-    "context"
-    "time"
+	"context"
 
-    "github.com/halokid/ColorfulRabbit"
-    kafka "github.com/segmentio/kafka-go"
+	"github.com/halokid/NeonRabbit/pkg"
+	kafka "github.com/segmentio/kafka-go"
 )
 
 type Kafka struct{}
 
 func NewKafka() *Kafka {
-    return &Kafka{}
+	return &Kafka{}
 }
 
 func (k *Kafka) Sub() error {
-    return nil
+	return nil
 }
 
 func (k *Kafka) Pub(topic, message string) error {
-    partition := 0
-    conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092",
-        topic, partition)
-    ColorfulRabbit.CheckError(err, "-->>> Kafka pub error")
 
-    conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
-    _, err = conn.WriteMessages(
-        kafka.Message{Value: []byte(message)},
-    )
-    ColorfulRabbit.CheckError(err, "-->>> Kafka pub err failed to write messages")
+	w := kafka.NewWriter(kafka.WriterConfig{
+		Brokers: []string{pkg.EnvGlobal.Broker.Server},
+		Topic:   topic,
+	})
+	ctx := context.Background()
+	err := w.WriteMessages(ctx, kafka.Message{
+		Value: []byte(message),
+	})
+	pkg.Logger.Errorf("-->>> Kafka pub err: %+v", err)
 
-    err = conn.Close()
-    ColorfulRabbit.CheckError(err, "-->>> Kafka pub failed to close writer")
-
-    return nil
+	return nil
 }
---
+++ pkg/logger.go
@@ -0,0 +1,32 @@
+package pkg
+
+import (
+	"log"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var Logger *zap.SugaredLogger
+
+func init() {
+	config := zap.NewProductionConfig()
+	config.EncoderConfig.TimeKey = "timestamp"
+	config.EncoderConfig.EncodeTime = zapcore.TimeEncoderOfLayout("Jan 02 15:04:05.000000000")
+	config.EncoderConfig.StacktraceKey = "" // to hide stacktrace info
+	// logLevel := zap.NewAtomicLevel()		// TODO: default log level is `info`
+	logLevel := zap.NewAtomicLevelAt(zapcore.WarnLevel) // TODO: set the log level to `warn`
+	config.Level = logLevel
+
+	logger, err := config.Build()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	Logger = logger.Sugar()
+
+	// log := logger.Sugar()
+	// log.Info("INFO log level message")
+	// log.Warn("Warn log level message")
+	// log.Error("Error log level message")
+}
