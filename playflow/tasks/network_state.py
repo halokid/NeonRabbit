a2266d130a18fc290792754d1e3a4b2fed3a66e8
mm jxx <r00txxyyzz@gmail.com>
2023-03-13 01:15:58 +0800

    playflow coding

--- playflow/tasks/network_state.py
+++ playflow/tasks/network_state.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
 
+from ..pkgx import common
 
 
+def run():
+  host_list = common.host_list
+
+  context.CLIARGS = ImmutableDict(
+    connection='smart', module_path=[
+      '/to/mymodules', '/usr/share/ansible'], forks=10, become=None,
+    become_method=None, become_user=None, check=False, diff=False,
+    verbosity=0
+  )
---
+++ playflow/tasks/tasks.py
@@ -0,0 +1,46 @@
+import json
+
+import ansible.constants as C
+from ansible.executor.task_queue_manager import TaskQueueManager
+from ansible.module_utils.common.collections import ImmutableDict
+from ansible.inventory.manager import InventoryManager
+from ansible.parsing.dataloader import DataLoader
+from ansible.playbook.play import Play
+from ansible.plugins.callback import CallbackBase
+from ansible.vars.manager import VariableManager
+from ansible import context
+
+# Create a callback plugin so we can capture the output
+class ResultsCollectorJSONCallback(CallbackBase):
+  """A sample callback plugin used for performing an action as results come in.
+
+  If you want to collect all results into a single object for processing at
+  the end of the execution, look into utilizing the ``json`` callback plugin
+  or writing your own custom callback plugin.
+  """
+
+  def __init__(self, *args, **kwargs):
+    super(ResultsCollectorJSONCallback, self).__init__(*args, **kwargs)
+    self.host_ok = {}
+    self.host_unreachable = {}
+    self.host_failed = {}
+
+  def v2_runner_on_unreachable(self, result):
+    host = result._host
+    self.host_unreachable[host.get_name()] = result
+
+  def v2_runner_on_ok(self, result, *args, **kwargs):
+    """Print a json representation of the result.
+
+    Also, store the result in an instance attribute for retrieval later
+    """
+    host = result._host
+    self.host_ok[host.get_name()] = result
+    print(json.dumps({host.name: result._result}, indent=4))
+
+  def v2_runner_on_failed(self, result, *args, **kwargs):
+    host = result._host
+    self.host_failed[host.get_name()] = result
+
+
+
